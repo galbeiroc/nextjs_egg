d75d6841f641e2bacdaa4a77adf4222882a65590
Albeiro Crespo Gutierrez <galbeiroc@Albeiros-MacBook-Pro.local>
2025-12-18 01:19:43 -0500

    work with link component

--- app-router/app/page.tsx
+++ app-router/app/page.tsx
@@ -1,28 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-center">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <img src="/logo.png" alt="Next.js Logo" className="w-28 h-28"/>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            Let&apos;s build something great together with Next.js!
           </p>
+          <p><Link href="/about">About Us</Link></p>
         </div>
       </main>
     </div>
