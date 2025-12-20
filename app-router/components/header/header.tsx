0d2123f24c229a1502eb508576d2c666b5ea83fc
Albeiro Crespo Gutierrez <galbeiroc@Albeiros-MacBook-Pro.local>
2025-12-19 21:12:49 -0500

    end header component

---
+++ app-router/components/header/header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import HeaderBackground from "./header-background";
+import logoImg from "@/assets/logo.png";
+import styles from "./header.module.css";
+
+export default function MainHeader() {
+  return (
+    <>
+      <HeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="Logo" loading="eager" />
+          Next Level Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="community">Foodies Commnunity</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
