a15dc959a8853aa0c3aa13b4edbae58b15167bff
Albeiro Crespo Gutierrez <galbeiroc@Albeiros-MacBook-Pro.local>
2025-12-21 10:11:39 -0500

    render meal page

---
+++ app-router/components/meals/meals-grid.tsx
@@ -0,0 +1,26 @@
+import MealItem from './meal-item';
+
+import styles from './meals-grid.module.css';
+
+export interface MealItem {
+  id: string;
+  title: string;
+  image: string;
+  summary: string;
+  createdBy: string;
+  slug: string;
+}
+
+interface MealsGridProps {
+  meals: MealItem[];
+}
+
+export default function MealsGrid({ meals }: MealsGridProps) {
+  return (
+    <ul className={styles.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}><MealItem {...meal} /></li>
+      ))}
+    </ul>
+  );
+}
